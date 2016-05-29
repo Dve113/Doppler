c50d9d1542393a50324733f5d03f88b9ea77fa0c
Dave113 <david.morawitzky@t-online.de>
2016-05-29 19:42:25 +0200

    Model Klassen hinzugefügt

---
+++ src/model/Firma.java
@@ -0,0 +1,89 @@
+package model;
+
+public class Firma {
+	String Firma;
+	String Telefon;
+	String Adresse1;
+	String Adresse2;
+	String Stadt;
+	String Bundesland;
+	String PLZ;
+	String Website;
+	String Branche;
+	String ID;
+	String Datensatzverwalter;
+	String Land;
+	public String getFirma() {
+		return Firma;
+	}
+	public void setFirma(String firma) {
+		Firma = firma;
+	}
+	public String getTelefon() {
+		return Telefon;
+	}
+	public void setTelefon(String telefon) {
+		Telefon = telefon;
+	}
+	public String getAdresse1() {
+		return Adresse1;
+	}
+	public void setAdresse1(String adresse1) {
+		Adresse1 = adresse1;
+	}
+	public String getAdresse2() {
+		return Adresse2;
+	}
+	public void setAdresse2(String adresse2) {
+		Adresse2 = adresse2;
+	}
+	public String getStadt() {
+		return Stadt;
+	}
+	public void setStadt(String stadt) {
+		Stadt = stadt;
+	}
+	public String getBundesland() {
+		return Bundesland;
+	}
+	public void setBundesland(String bundesland) {
+		Bundesland = bundesland;
+	}
+	public String getPLZ() {
+		return PLZ;
+	}
+	public void setPLZ(String pLZ) {
+		PLZ = pLZ;
+	}
+	public String getWebsite() {
+		return Website;
+	}
+	public void setWebsite(String website) {
+		Website = website;
+	}
+	public String getBranche() {
+		return Branche;
+	}
+	public void setBranche(String branche) {
+		Branche = branche;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getDatensatzverwalter() {
+		return Datensatzverwalter;
+	}
+	public void setDatensatzverwalter(String datensatzverwalter) {
+		Datensatzverwalter = datensatzverwalter;
+	}
+	public String getLand() {
+		return Land;
+	}
+	public void setLand(String land) {
+		Land = land;
+	}
+	
+}
---
+++ src/model/Kontakt.java
@@ -0,0 +1,68 @@
+package model;
+
+public class Kontakt {
+
+	String Firma;
+	String Kontakt;
+	String Telefon;
+	String Adresse;
+	String Adresse2;
+	String Stadt;
+	String PLZ;
+	String EMail;
+	String Erstellungsdatum;
+	public String getFirma() {
+		return Firma;
+	}
+	public void setFirma(String firma) {
+		Firma = firma;
+	}
+	public String getKontakt() {
+		return Kontakt;
+	}
+	public void setKontakt(String kontakt) {
+		Kontakt = kontakt;
+	}
+	public String getTelefon() {
+		return Telefon;
+	}
+	public void setTelefon(String telefon) {
+		Telefon = telefon;
+	}
+	public String getAdresse() {
+		return Adresse;
+	}
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
+	}
+	public String getAdresse2() {
+		return Adresse2;
+	}
+	public void setAdresse2(String adresse2) {
+		Adresse2 = adresse2;
+	}
+	public String getStadt() {
+		return Stadt;
+	}
+	public void setStadt(String stadt) {
+		Stadt = stadt;
+	}
+	public String getPLZ() {
+		return PLZ;
+	}
+	public void setPLZ(String pLZ) {
+		PLZ = pLZ;
+	}
+	public String getEMail() {
+		return EMail;
+	}
+	public void setEMail(String eMail) {
+		EMail = eMail;
+	}
+	public String getErstellungsdatum() {
+		return Erstellungsdatum;
+	}
+	public void setErstellungsdatum(String erstellungsdatum) {
+		Erstellungsdatum = erstellungsdatum;
+	}
+}
